5550df72431374ddaa0342191b31ec993e82b53e
李满满 <xmdevops@vip.qq.com>
2017-12-12 19:56:09 +0800

    :underage: fix read body with Content-Length

--- simple_http_parser/http.py
+++ simple_http_parser/http.py
@@ -10,7 +10,8 @@ import re
 
 
 class HttpStream(object):
-    __vre = re.compile("HTTP/(?P<p>\d+).(?P<m>\d+) (?P<n>\d{3}) (?P<s>\w+)")
+    __len = re.compile(r'Content-Length: (?P<n>[0-9]+)')
+    __vre = re.compile(r'HTTP/(?P<p>\d+).(?P<m>\d+) (?P<n>\d{3}) (?P<s>\w+)')
     __buf = ''
     __res = []
 
@@ -21,6 +22,16 @@ class HttpStream(object):
         match = re.search(self.__class__.__vre, self.__class__.__buf)
 
         return match
+
+    def __content_length_match(self, header):
+        match = re.search(self.__class__.__len, header)
+
+        return match
+
+    def __content_length_expand(self, match):
+        tpl = '\g<n>'
+
+        return match.expand(tpl)
 
     def __version_match_expand(self, match):
         tpl = 'HTTP/\g<p>.\g<m> \g<n> \g<s>'
@@ -41,11 +52,28 @@ class HttpStream(object):
         strs_list = self.__class__.__buf.split(match_str)
 
         data_dpos = 0
+        """
+        1. 等于一个HTTP/1.1 200 OK,需要特殊处理
+            1. 存在\r\n\r\n时
+            2. 不存在\r\n\r\n\时
+        2. 大于一个HTTP/1.1 200 OK
+        """
         for item in strs_list:
             data_dpos = data_dpos + len(match_str) + 2 + len(item)
             if end_flag in item:
-                json_data = item.split(end_flag)[-1]
-                self.__class__.__res.append(json_data)
+                header, body = item.split(end_flag)
+                match = self.__content_length_match(header)
+                if not match:
+                    data_dpos = 0
+                    continue
+                content_length = int(self.__content_length_expand(match))
+                if len(body.strip()) < content_length:
+                    data_dpos = 0
+                    continue
+                self.__class__.__res.append(body[:content_length])
+            else:
+                data_dpos = 0
+
         self.__class__.__buf = self.__class__.__buf[data_dpos:]
 
     def read(self, buffer_size=4096):
